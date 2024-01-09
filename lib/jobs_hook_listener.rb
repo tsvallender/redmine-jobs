80d3bc8454e74344e827722c914d84a25cae06b0
Trevor Vallender <trevor@tsvallender.co.uk>
2024-01-09 08:43:07 +0000

    Correct view dir name
    Refs #2188

--- lib/jobs_hook_listener.rb
+++ lib/jobs_hook_listener.rb
@@ -1,5 +1,5 @@
 class JobsHookListener < Redmine::Hook::ViewListener
-  render_on :view_timelog_edit_form_bottom, partial: "timelogs/edit_form_bottom"
+  render_on :view_timelog_edit_form_bottom, partial: "time_entries/edit_form_bottom"
   render_on :view_users_form, partial: "users/edit_form"
 end
 
