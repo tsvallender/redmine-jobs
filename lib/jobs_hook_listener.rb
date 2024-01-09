bf2a2074292c3f4a1f569106afc7dcc8787a77fa
Trevor Vallender <trevor@tsvallender.co.uk>
2024-01-09 09:20:44 +0000

    Allow bulk edit of job on time entries
    Refs #2188

--- lib/jobs_hook_listener.rb
+++ lib/jobs_hook_listener.rb
@@ -1,4 +1,5 @@
 class JobsHookListener < Redmine::Hook::ViewListener
+  render_on :view_time_entries_bulk_edit_details_bottom, partial: "time_entries/bulk_edit_details_bottom"
   render_on :view_timelog_edit_form_bottom, partial: "time_entries/edit_form_bottom"
   render_on :view_users_form, partial: "users/edit_form"
 end
